711f825a51da62944e5832ad08521324001284b6
LorenzoCeccarelli <56482293+LorenzoCeccarelli@users.noreply.github.com>
2021-05-27 17:00:42 +0200

    feat: update docs images

--- src/main/java/clientsideEncryption/examples/CreateTableAndQueryItExample.java
+++ src/main/java/clientsideEncryption/examples/CreateTableAndQueryItExample.java
@@ -7,7 +7,12 @@ import clientsideEncryption.core.exceptions.ConfigurationFileError;
 import clientsideEncryption.core.exceptions.InitializationError;
 import clientsideEncryption.core.exceptions.InvalidQueryException;
 import clientsideEncryption.core.exceptions.QueryExecutionError;
+import com.sun.management.OperatingSystemMXBean;
 
+import javax.management.MBeanServerConnection;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.sql.SQLException;
 import java.util.Set;
 
 public class CreateTableAndQueryItExample {
@@ -16,6 +21,16 @@ public class CreateTableAndQueryItExample {
             CryptoDatabaseAdapter cda = new CryptoDatabaseAdapter.Builder()
                     .buildByFile("src/main/resources/config.properties");
             cda.init();
+
+            MBeanServerConnection mbsc = ManagementFactory.getPlatformMBeanServer();
+
+            OperatingSystemMXBean osMBean = ManagementFactory.newPlatformMXBeanProxy(
+                    mbsc, ManagementFactory.OPERATING_SYSTEM_MXBEAN_NAME, OperatingSystemMXBean.class);
+
+            long nanoBefore = System.nanoTime();
+            long cpuBefore = osMBean.getProcessCpuTime();
+            System.out.println(nanoBefore/1000000);
+            System.out.println(cpuBefore/1000000);
             cda.newQueryBuilder("drop table if exists users").run();
             cda.newQueryBuilder("create table users(" +
                                                     "id varchar(255) primary key, " +
@@ -28,23 +43,28 @@ public class CreateTableAndQueryItExample {
                     .setParameter(1,"1")
                     .setParameter(2,"Josh")
                     .setCipherParameter(3,"54589720575", CryptoUtils.Algorithm.AES192)
-                    .run();
+                    .addToBatch();
             cda.newQueryBuilder("insert into users(id,name,creditCardNumber)" +
                     "values(?,?,?)")
                     .setParameter(1,"2")
                     .setParameter(2,"Mary")
                     .setCipherParameter(3,"786787575", CryptoUtils.Algorithm.AES128)
-                    .run();
+                    .addToBatch();
             cda.newQueryBuilder("insert into users(id,name,creditCardNumber)" +
                     "values(?,?,?)")
                     .setParameter(1,"3")
                     .setParameter(2,"Luke")
                     .setCipherParameter(3,"90904534", CryptoUtils.Algorithm.AES128)
-                    .run();
+                    .addToBatch();
+            cda.executeBatch();
 
+            long nanoAfter = System.nanoTime();
+            long cpuAfter = osMBean.getProcessCpuTime();
+            System.out.println("Elapsed Time(ms): " + (nanoAfter-nanoBefore)/(1000000));
+            System.out.println("CPU Time: " + (cpuAfter-cpuBefore)/1000000);
             Set<Tuple> result = cda.newQueryBuilder("select * from users").runSelect();
             result.forEach(System.out::println);
-        } catch (ConfigurationFileError | QueryExecutionError | InitializationError | InvalidQueryException configurationFileError) {
+        } catch (ConfigurationFileError | QueryExecutionError | InitializationError | InvalidQueryException | IOException | SQLException configurationFileError) {
             configurationFileError.printStackTrace();
         }
     }
