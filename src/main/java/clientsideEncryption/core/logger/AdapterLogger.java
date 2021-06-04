b73b17e0391622897be73f6f19e295a3c83f3dae
LorenzoCeccarelli <56482293+LorenzoCeccarelli@users.noreply.github.com>
2021-06-04 15:15:28 +0200

    feat: update logFilePath

--- src/main/java/clientsideEncryption/core/logger/AdapterLogger.java
+++ src/main/java/clientsideEncryption/core/logger/AdapterLogger.java
@@ -6,13 +6,14 @@ import java.util.logging.*;
 public class AdapterLogger {
     static private FileHandler fileTxt;
     static private Formatter formatterTxt;
+    private String logfilePath;
 
-    static public void setup() throws IOException {
+    static public void setup(String logfilePath) throws IOException {
         Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
         Logger rootLogger = Logger.getLogger("");
         rootLogger.removeHandler(rootLogger.getHandlers()[0]);
         logger.setLevel(Level.INFO);
-        fileTxt = new FileHandler("src/main/resources/log.txt");
+        fileTxt = new FileHandler(logfilePath);
         formatterTxt = new CustomFormatter();
         fileTxt.setFormatter(formatterTxt);
         logger.addHandler(fileTxt);
